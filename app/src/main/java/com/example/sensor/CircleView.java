b9087801362361b298c8c7714c5d7b8738b12037
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-13 15:18:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sensor/CircleView.java
@@ -0,0 +1,10 @@
+package com.example.sensor;
+
+import android.content.Context;
+import android.view.View;
+
+public class CircleView extends View {
+    public CircleView(Context context) {
+        super(context);
+    }
+}
