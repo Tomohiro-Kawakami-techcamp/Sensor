8887c7777a2ebc755fcd47acd86b3b70367f8014
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-13 15:42:12 +0900

    長さ等設定

--- app/src/main/java/com/example/sensor/CircleView.java
+++ app/src/main/java/com/example/sensor/CircleView.java
@@ -1,10 +1,32 @@
 package com.example.sensor;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.view.View;
 
 public class CircleView extends View {
+
+    private static final int DIAMETER = 200;
+    private static final int OFFSET_X = 0;
+    private static final int OFFSET_Y = 100;
+
+    private ShapeDrawable ball;
+
+    private int ball_X;
+    private int ball_Y;
+
+    private int width;
+    private int height;
+
     public CircleView(Context context) {
         super(context);
+
+        ball = new ShapeDrawable(new OvalShape());
+        ball.getPaint().setColor(Color.RED);
+
+        ball_X = 200;
+        ball_Y = 200;
     }
 }
